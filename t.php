9a4fef200fbe70a5435bc4cd8ae4f607d313265b
pp <istvan@palocz.hu>
2011-10-04 14:11:21 +0200

    Kapcsoljuk ki a flag modult

--- t.php
+++ t.php
@@ -3,7 +3,7 @@ define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
-
+db_query("UPDATE {system} SET status = 0 WHERE name='flag'");
 $result = db_select('system', s)->fields('s')->condition('status', 1)->execute();
 print '<pre>';
 foreach($result as $row) {
