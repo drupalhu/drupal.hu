1e9616b7d75eee14f7467d178fc2e5bd70729278
István Palócz <istvan@palocz.hu>
2014-12-17 16:30:08 +0100

    Clarify comments

--- hooks/scripts/enable-modules.sh
+++ hooks/scripts/enable-modules.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 #
-# Cloud Hook: post-db-copy
+# Hook helper script. Enable necessary modules.
 #
-# The post-db-copy hook is run whenever you use the Workflow page to copy a
-# database from one environment to another. See ../README.md for
-# details.
-#
-# Usage: post-db-copy site target-env db-name source-env
+# Usage: enable-modules.sh site target-env db-name source-env
 
 site="$1"
 target_env="$2"
