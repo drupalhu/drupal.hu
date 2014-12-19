fc2747b08dd993fe392aecb3edfad797db48335f
Balazs Dianiska <balazs.dianiska@acquia.com>
2014-12-19 15:43:29 +0100

    Re-adding the per environment script running as the common scripts run second.

--- hooks/test/post-db-copy/run.sh
+++ hooks/test/post-db-copy/run.sh
@@ -7,4 +7,6 @@ source_env="$4"
 
 drush_alias=$site'.'$target_env
 
+. `dirname $0`/../../scripts/update-db.sh $drush_alias
+. `dirname $0`/../../scripts/enable-modules.sh $drush_alias
 . `dirname $0`/../../scripts/drush-cache-clear.sh $drush_alias
