aabd62fde14d91ea166e0f866280b0994a30d0ce
Balazs Dianiska <balazs.dianiska@acquia.com>
2014-12-19 14:59:55 +0100

    A bit more elegant path referencing.

--- hooks/test/post-code-deploy/run.sh
+++ hooks/test/post-code-deploy/run.sh
@@ -19,6 +19,4 @@ repo_type="$6"
 
 drush_alias=$site'.'$target_env
 
-# Get the current directory
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-sh ${DIR}/../../scripts/drush-cache-clear.sh $drush_alias
+. `dirname $0`/../../scripts/drush-cache-clear.sh $drush_alias
