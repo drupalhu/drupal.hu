5c1191b2d2a0bb7c9f990dfdf9ac61630eabc8df
Adam Boros <hunaboros@gmail.com>
2015-05-27 11:02:41 +0200

    Call registry rebuild from cloud hooks.

--- hooks/test/post-code-deploy/run.sh
+++ hooks/test/post-code-deploy/run.sh
@@ -19,6 +19,7 @@ repo_type="$6"
 
 drush_alias=$site'.'$target_env
 
+. `dirname $0`/../../scripts/drush-registry-rebuild.sh $drush_alias
 . `dirname $0`/../../scripts/update-db.sh $drush_alias
 . `dirname $0`/../../scripts/enable-modules.sh $drush_alias
 . `dirname $0`/../../scripts/drush-cache-clear.sh $drush_alias
