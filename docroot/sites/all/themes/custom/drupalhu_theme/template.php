155e7a1e7e89506bd7ee5c8ca022363e8ef4aeda
Istvan Csaki <steven.csaki@gmail.com>
2015-10-06 19:41:06 +0200

    Include inc files

--- docroot/sites/all/themes/custom/drupalhu_theme/template.php
+++ docroot/sites/all/themes/custom/drupalhu_theme/template.php
@@ -4,6 +4,50 @@
  * template.php
  */
 
+
+// Include .inc files.
+_drupalhu_theme_include_inc_files();
+
+/**
+ * Include all .inc files from includes directory.
+ */
+function _drupalhu_theme_include_inc_files() {
+  $theme_path = drupal_get_path('theme', 'drupalhu_theme');
+  $files = file_scan_directory($theme_path . '/includes', '/\.inc$/');
+
+  foreach ($files as $path) {
+    $is_module = 'modules' == basename(dirname($path->uri));
+    // Auto include file if not in modules dir.
+    // Include files from modules dir if module is exists.
+    if (
+      !$is_module
+      || ($is_module && module_exists($path->name))
+    ) {
+      require_once $path->uri;
+    }
+  }
+}
+
+/**
+ * Get theme hooks.
+ */
+function _drupalhu_theme_hooks($hook) {
+  $hooks = &drupal_static(__FUNCTION__, array());
+  if (!isset($hooks[$hook])) {
+    $hooks[$hook] = array();
+    $prefix = "_drupalhu_theme_{$hook}__";
+    $all_functions = get_defined_functions();
+    foreach ($all_functions['user'] as $func) {
+      if (0 !== strpos($func, $prefix)) {
+        continue;
+      }
+      $hooks[$hook][] = $func;
+    }
+  }
+
+  return $hooks[$hook];
+}
+
 /**
  * Implements hook_preprocess_html().
  */
