1a218e8e99c2b3b3a51beae028b118cf4668e85f
Istvan Csaki <steven.csaki@gmail.com>
2016-05-20 11:46:26 +0200

    Delete breadcrumb hook, #refactor

--- docroot/sites/all/themes/demon/template.php
+++ docroot/sites/all/themes/demon/template.php
@@ -143,26 +143,6 @@ function demon_preprocess_maintenance_page(&$variables) {
 }
 
 /**
- * Add current page to end of breadcrumb.
- */
-function demon_breadcrumb($variables) {
-  $breadcrumb = $variables['breadcrumb'];
-  if (!empty($breadcrumb)) {
-    // Adding the title of the current page to the breadcrumb.
-    $breadcrumb[] = drupal_get_title();
-
-    // Provide a navigational heading to give context for breadcrumb links to
-    // screen-reader users. Make the heading invisible with .element-invisible.
-    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
-
-    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
-    return $output;
-  }
-
-  return NULL;
-}
-
-/**
  * Implements hook_theme().
  */
 function demon_theme($existing, $type, $theme, $path) {
@@ -255,7 +235,7 @@ function job_link($variables) {
   }
   else {
     print l(t('Add new job'), 'user/login', array(
-        'query' => array('destination' => 'node/add/job'), 
+        'query' => array('destination' => 'node/add/job'),
         'attributes' => array('class' => array('button-action'))
       )
     );
