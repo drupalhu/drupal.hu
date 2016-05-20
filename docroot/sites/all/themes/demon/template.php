69204bfc4bff07b4c10d162d23c3bc1c88866e3f
Istvan Csaki <steven.csaki@gmail.com>
2016-05-20 21:53:51 +0200

    Use new layout on site, #refactor

--- docroot/sites/all/themes/demon/template.php
+++ docroot/sites/all/themes/demon/template.php
@@ -22,6 +22,17 @@ function demon_preprocess_page(&$variables) {
       $variables['sidebar_classes_array'][] = 'grid-8';
     }
   }
+
+  // Add information about the number of sidebars.
+  if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    $variables['content_column_class'] = ' class="col-sm-6"';
+  }
+  elseif (!empty($variables['page']['sidebar_first']) || !empty($variables['page']['sidebar_second'])) {
+    $variables['content_column_class'] = ' class="col-sm-8"';
+  }
+  else {
+    $variables['content_column_class'] = ' class="col-sm-12"';
+  }
 }
 
 /**
