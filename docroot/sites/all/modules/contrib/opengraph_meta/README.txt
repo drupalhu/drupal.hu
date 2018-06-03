acfd70842e932e807e4eb45a1aa5da195123abb4
Dávid Andor <david-andor@kozpontiagy.hu>
2018-06-03 14:00:34 +0200

    Clean slate for D8

--- docroot/sites/all/modules/contrib/opengraph_meta/README.txt
+++
@@ -1,5 +0,0 @@
-====================
-Open Graph meta tags
-====================
-
-Please see http://drupal.org/project/opengraph_meta for up-to-date notes.
--- hooks/scripts/feature-revert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Hook helper script. Enable necessary modules.
-#
-# Usage: enable-modules.sh site target-env db-name source-env
-
-drush_alias=$1
-
-drush @$drush_alias features-revert-all --yes
