f3bdef5986f25fad6e10dfe34a0710baabccb143
Dávid Andor <david-andor@kozpontiagy.hu>
2021-10-14 03:32:00 +0200

    Issue #0 - Split teaser and body in legacy migration

---
+++ docroot/modules/custom/app_dc/src/Plugin/migrate/process/NonEmpty.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\app_dc\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * @\Drupal\migrate\Annotation\MigrateProcessPlugin(
+ *   id = "app_non_empty"
+ * )
+ */
+class NonEmpty extends ProcessPluginBase {
+
+  protected function getDefaultConfiguration(): array {
+    return [
+      'offset' => 0,
+      'strict' => FALSE,
+    ];
+  }
+
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $config = array_replace_recursive($this->getDefaultConfiguration(), $this->configuration);
+
+    $items = array_values(array_filter(
+      $value,
+      function ($item) use ($config) {
+        return ($config['strict'] && isset($item)) || !empty($item);
+      },
+    ));
+
+    return $items[$config['offset']] ?? NULL;
+  }
+
+}
